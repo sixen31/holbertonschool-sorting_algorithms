e288b7f6f6e3906e79df2a4c1f3400116306e4a8
sixen31 <alexisgrandjean@hotmail.fr>
2023-05-24 10:26:52 -0700

    Modification betty style

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -2,8 +2,8 @@
 
 /**
  *Fonction de swap pour échanger les valeurs de deux entiers.
- *@param a Pointeur vers le premier entier.
- *@param b Pointeur vers le deuxième entier.
+ * @param a Pointeur vers le premier entier.
+ * @param b Pointeur vers le deuxième entier.
  */
 
 void swap(int *a, int *b)
@@ -15,10 +15,10 @@ void swap(int *a, int *b)
 
 /**
  *Fonction de partitionnement pour le tri rapide.
- *@param array Tableau d'entiers à trier.
- *@param low Indice de début de la partition.
- *@param high Indice de fin de la partition.
- *@return L'indice du pivot après partitionnement.
+ * @param array Tableau d'entiers à trier.
+ * @param low Indice de début de la partition.
+ * @param high Indice de fin de la partition.
+ * @return L'indice du pivot après partitionnement.
  */
 
 int partition(int *array, int low, int high)
@@ -41,9 +41,9 @@ int partition(int *array, int low, int high)
 
 /**
  *Fonction auxiliaire pour effectuer le tri rapide de manière récursive.
- *@param array Tableau d'entiers à trier.
- *@param low Indice de début de la partition.
- *@param high Indice de fin de la partition.
+ * @param array Tableau d'entiers à trier.
+ * @param low Indice de début de la partition.
+ * @param high Indice de fin de la partition.
  */
 
 void quick_sort_helper(int *array, int low, int high)
@@ -59,8 +59,8 @@ void quick_sort_helper(int *array, int low, int high)
 
 /**
  *Fonction principale pour effectuer le tri rapide sur un tableau.
- *@param array Tableau d'entiers à trier.
- *@param size Taille du tableau.
+ * @param array Tableau d'entiers à trier.
+ * @param size Taille du tableau.
  */
 
 void quick_sort(int *array, size_t size)
