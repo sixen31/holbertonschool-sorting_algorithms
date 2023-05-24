89d065adc6364526856044ce0ea05be42e355bcb
sixen31 <alexisgrandjean@hotmail.fr>
2023-05-24 04:52:44 -0700

    modification condition

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -2,8 +2,8 @@
 
 /*
  * insertion_sort_list - Function that sorts a doubly linked list of integers
- *                       in ascending order using the Insertion sort algorithm.
- * @list: Double pointer to the head of the doubly linked list.
+ * in ascending order using the Insertion sort algorithm.
+ *@list: Double pointer to the head of the doubly linked list.
  *
  * Description: This function implements the Insertion sort algorithm to sort
  * a doubly linked list in ascending order. It takes the double pointer to the
@@ -15,7 +15,7 @@ void insertion_sort_list(listint_t **list)
 	listint_t *current = (*list)->next;
 	listint_t *temp = NULL;
 
-	if (*list == NULL || list == NULL || (*list)->next == NULL)
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;	
 
 	while (current != NULL)
