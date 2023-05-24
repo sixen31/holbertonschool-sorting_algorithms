7b35bbb0517c4416406487adc78213a17a5c9030
sixen31 <alexisgrandjean@hotmail.fr>
2023-05-24 05:42:27 -0700

    gestion erreur

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,32 +1,31 @@
 #include "sort.h"
-
 /**
- * insertion_sort_list - Sorts a double-chained list insertion sort algorithm.
+ * insertion_sort_list - Sorts a double-chained list using the insertion sort algorithm.
  *
- *@list: A pointer to a pointer to the first element in the list.
+ * @list: A pointer to a pointer to the first element in the list.
  *
  * Description :
- * This function sorts a double-chained list of the insertion sort algorithm.
- * It traverses the list by comparing with the previous element and reorganize
- * Rearrange the links accordingly to insert each element in its correct place
+ * This function sorts a double-chained list using the insertion sort algorithm.
+ * It traverses the list by comparing each element with the previous elements and rearranges the links
+ * Rearrange the links accordingly to insert each element in its correct place.
  */
-
 void insertion_sort_list(listint_t **list)
 {
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+		return;
+
 	listint_t *current = (*list)->next;
 	listint_t *temp = NULL;
-
-	if (list == NULL || *list == NULL)
-		return;
 
 	while (current != NULL)
 	{
 		temp = current;
 		while (temp->prev != NULL && temp->n < temp->prev->n)
 		{
+			temp->prev->next = temp->next;
+
 			if (temp->next != NULL)
 				temp->next->prev = temp->prev;
-			temp->prev->next = temp->next;
 
 			temp->next = temp->prev;
 			temp->prev = temp->prev->prev;
