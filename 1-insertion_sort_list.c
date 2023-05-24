42cbd1202eab97f5fe81f14b54efab8bacbe6236
sixen31 <alexisgrandjean@hotmail.fr>
2023-05-24 00:58:46 -0700

    Modification correct code and declaration

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -9,11 +9,12 @@
  */
 void insertion_sort_list(listint_t **list)
 {
+	listint_t *current = (*list)->next;
+	listint_t *temp = NULL;
+
 	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
 
-	listint_t *current = (*list)->next;
-	listint_t *temp = NULL;
 
 	while (current != NULL)
 	{
