1d095c143a53ee55e3bcbcbef89be62d43bd2c75
sixen31 <alexisgrandjean@hotmail.fr>
2023-05-24 10:12:22 -0700

    modification declaration

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -11,8 +11,9 @@ int partition(int *array, int low, int high)
 {
 	int pivot = array[high];
 	int i = low - 1;
+	int j;
 
-	for (int j = low; j <= high - 1; j++)
+	for (j = low; j <= high - 1; j++)
 	{
 		if (array[j] < pivot)
 		{
