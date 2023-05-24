fe80972de1a3ff5d028e02928b05552b6b6d5d82
sixen31 <alexisgrandjean@hotmail.fr>
2023-05-24 05:35:17 -0700

    betty style

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
 /**
- * selection_sort - Sorts an array of integers in ascending order using the Selection sort algorithm
+ * selection_sort - Sorts an array  in ascending Selection sort algorithm
  *
  * @array: The array to be sorted
  * @size: The size of the array
  */
+
 void selection_sort(int *array, size_t size)
 {
 	size_t i, j, min_idx;
@@ -29,7 +30,6 @@ void selection_sort(int *array, size_t size)
 			array[i] = array[min_idx];
 			array[min_idx] = temp;
 
-			/* Print the array after each swap */
 			for (j = 0; j < size; j++)
 			{
 				if (j != 0)
