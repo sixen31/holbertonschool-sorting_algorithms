4bd0d42aaf93f12c217c8689d3cee3add49a0452
sixen31 <alexisgrandjean@hotmail.fr>
2023-05-24 01:33:29 -0700

    deplace verification debut code

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -9,36 +9,33 @@
  */
 void insertion_sort_list(listint_t **list)
 {
-        listint_t *current = (*list)->next;
-        listint_t *temp = NULL;
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+		return;
 
-        if (list == NULL || *list == NULL || (*list)->next == NULL)
-                return;
+	listint_t *current = (*list)->next;
+	listint_t *temp = NULL;
 
+	while (current != NULL)
+	{
+		temp = current;
+		while (temp->prev != NULL && temp->n < temp->prev->n)
+		{
+			if (temp->next != NULL)
+				temp->next->prev = temp->prev;
+			temp->prev->next = temp->next;
 
-        while (current != NULL)
-        {
-                temp = current;
-                while (temp->prev != NULL && temp->n < temp->prev->n)
-                {
-                        if (temp->next != NULL)
-                                temp->next->prev = temp->prev;
-                        temp->prev->next = temp->next;
+			temp->next = temp->prev;
+			temp->prev = temp->prev->prev;
+			temp->next->prev = temp;
 
-                        temp->next = temp->prev;
-                        temp->prev = temp->prev->prev;
-                        temp->next->prev = temp;
+			if (temp->prev == NULL)
+				*list = temp;
+			else
+				temp->prev->next = temp;
 
-                        if (temp->prev == NULL)
-                                *list = temp;
-                        else
-                                temp->prev->next = temp;
+			print_list(*list);
+		}
 
-                        print_list(*list);
-                }
-                if (current->next != NULL)
-                        current = current->next;
-       		else
-			break;
-       	}
+		current = current->next;
+	}
 }
