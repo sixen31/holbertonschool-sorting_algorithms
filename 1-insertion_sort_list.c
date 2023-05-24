49b7c6c19b1144a1b94265aa6f9bdbad61ab4036
sixen31 <alexisgrandjean@hotmail.fr>
2023-05-24 03:04:22 -0700

    Gestion Code

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -9,18 +9,16 @@
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *current = NULL;
-    listint_t *temp = NULL;
+	listint_t *current = (*list)->next;
+	listint_t *temp = NULL;
 
 	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
 
-	current = (*list)->next;
 
 	while (current != NULL)
 	{
 		temp = current;
-
 		while (temp->prev != NULL && temp->n < temp->prev->n)
 		{
 			if (temp->next != NULL)
