d6fff867fb72f3df689d3cd17d33f3d5a1185eda
alex <6267@holbertonstudents.com>
2023-09-21 11:48:57 -0700

    reprise code de base

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,7 +1,5 @@
 #include "sort.h"
 
-void quick_sort_helper(int *array, int low, int high, size_t size);
-
 /**
  * swap - Swaps two integers in an array
  *
@@ -61,7 +59,6 @@ void quicksort(int *array, int low, int high, size_t size)
 
 	if (low < high)
 	{
-
 		quicksort(array, low, pi - 1, size);
 		quicksort(array, pi + 1, high, size);
 	}
@@ -76,30 +73,8 @@ void quicksort(int *array, int low, int high, size_t size)
  */
 void quick_sort(int *array, size_t size)
 {
-	int pi = partition(array, 0, size - 1, size);
-
 	if (array == NULL || size < 2)
 		return;
 
-	quicksort(array, 0, pi - 1, size);
-	quicksort(array, pi + 1, size - 1, size);
-}
-
-/**
- * quick_sort_helper - Helper function for quick_sort
- *
- * @array: Array to be sorted
- * @low: Starting index of the partition to be sorted
- * @high: Ending index of the partition to be sorted
- * @size: Size of the array
- */
-void quick_sort_helper(int *array, int low, int high, size_t size)
-{
-	if (low < high)
-	{
-		int pi = partition(array, low, high, size);
-
-		quick_sort_helper(array, low, pi - 1, size);
-		quick_sort_helper(array, pi + 1, high, size);
-	}
+	quicksort(array, 0, size - 1, size);
 }
